6715c40ccfd8f83e61278849600499f2b7e4ae2e
jajav33 <to.jajav33@gmail.com>
2020-07-18 13:37:56 -0700

    Player moves distance of mouse to itself

--- src/components/Player.gd
+++ src/components/Player.gd
@@ -2,27 +2,36 @@ extends Node2D
 
 
 # Declare member variables here. Examples:
-var __diveSpeed : float = 300.0;
+const __DIVE_DIST_FACTOR := 0.25;
+const __DIVE_MAX_DIST := 1000.0;
+var __diveSpeed := 0.0;
+var __diveDirection := Vector2.ZERO;
+var __willDiveOnRelease := false;
+var __canUseInput = true;
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_process_input(true);
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-func _input(event: InputEvent) -> void:
-	if (event.is_action_pressed("dive")):
+func _process(delta: float) -> void:
+	self.__canUseInput = ($RigidBody2D.linear_velocity == Vector2.ZERO) or ($RigidBody2D.sleeping);
+	
+	if ((self.__canUseInput == true) && Input.is_action_pressed("dive")):
+		self.__willDiveOnRelease = true;
+		var space_state = get_world_2d().direct_space_state;
+		
+		var startPos : Vector2 = $RigidBody2D.global_position;
+		var endPos : Vector2 = self.get_global_mouse_position();
+		var distance = min(startPos.distance_to(endPos), self.__DIVE_MAX_DIST);
+		self.__diveSpeed = self.__DIVE_DIST_FACTOR * distance;
+		
+	elif((self.__canUseInput == true) && (self.__willDiveOnRelease)):
+		self.__willDiveOnRelease = false;
 		self.fnDive(Vector2.RIGHT * self.__diveSpeed);
-	return;
+	pass
 
 func fnDive (paramForce : Vector2) -> void:
-	var canDive = true;
-	
-	if (canDive):
-		$RigidBody2D.apply_central_impulse(paramForce);
-		canDive = false;
+	$RigidBody2D.apply_central_impulse(paramForce);
 	return;
